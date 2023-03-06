16a8668fbe561fa856df419e6c3bae0db8e4d93b
309856352@qq.com <309856352@qq.com>
2023-03-06 20:10:03 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+// 配置信息
+module.exports = {
+  fromDisplayText: '孙少', // 收件箱展示的来件人名字
+  fromDisplaySubText: '每日提醒', // 收件箱展示的次级标题
+  user: '309856352@qq.com', // 发送者邮箱
+  pass: 'maplirjzenlcbhcj', // 发送者邮箱MTP协议密码
+  to: '309856352@qq.com', // 发送到谁，填邮箱
+  weatherKey: '9e1acc650bfc48b080d3ce8ff0e5b82e', // 和风天气key
+  location: '101270201',  // 和风天气-四川攀枝花id
+  type: '1,3,9', // 和风天气-生活指数type
+  tianXingKey: '3588dcb6cfc88f4aaa84fc0102a887ed', // 天行数据的key
+  startDay: '2023-03-02', // 在一起的日期
+}

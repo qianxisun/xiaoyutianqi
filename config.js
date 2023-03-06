9ad6d55b228864c301e8ab5e9f5b2cdf942c0493
qianxisun <77652542+qianxisun@users.noreply.github.com>
2023-03-06 20:47:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   fromDisplaySubText: '每日提醒', // 收件箱展示的次级标题
   user: '309856352@qq.com', // 发送者邮箱
   pass: 'maplirjzenlcbhcj', // 发送者邮箱MTP协议密码
-  to: '309856352@qq.com', // 发送到谁，填邮箱
+  to: '3533416495@qq.com', // 发送到谁，填邮箱
   weatherKey: '9e1acc650bfc48b080d3ce8ff0e5b82e', // 和风天气key
   location: '101270201',  // 和风天气-四川攀枝花id
   type: '1,3,9', // 和风天气-生活指数type
